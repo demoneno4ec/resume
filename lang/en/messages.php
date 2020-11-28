95ed9d26a3fa4f31c8a0a01499ba89a31b9efdd4
pestovmv <pestovmv@inbox.ru>
2020-11-29 01:41:38 +0300

    sync local version with repository

---
+++ lang/en/messages.php
@@ -0,0 +1,157 @@
+<?php
+
+return $messages = [
+    'name'       => 'Maxim Pestov',
+    'age'        => '29 age.',
+    'birthday'   => [
+        'title' => 'Date of birth:',
+        'value' => '08.05.1991',
+    ],
+    'experience' => [
+        'title' => 'Experience in IT:',
+        'value' => 'from 2017 year',
+    ],
+    'about'      => [
+        'title' => 'About me',
+        'value' => '
+I feel confident in any situation. Always ready for change.<br/>
+I achieve my goals..<br/>
+I competently approach the allocation of resources to achieve the final result..<br/>
+I love people who love their job and I believe that every person wants development..<br/>
+I try to avoid stressful situations, but I also know the ways and stages of getting out of them..<br/>
+I take responsibility for the work done..<br/>
+In my spare time:.<br/>
+- read the book 
+<a href="https://g.co/kgs/6tF9NZ">
+    "Perfect Code"
+</a>.<br/>
+- ride a bike;.<br/>
+- walk around the city;.<br/>
+- read comments on 
+<a href="https://habr.com/ru/">
+    habr.com
+</a>.<br/>
+<br/>
+one of my qualities is the ability to organize a workflow.<br/>
+<br/>
+What makes me different from others?.<br/>
+- Perseverance and patience, I can easily do monotonous work.<br/>
+- Responsibly approach the performance of any task, I complete all assigned tasks conscientiously.<br/>
+- Fast learning, easily assimilate new information.<br/>
+- I can listen and hear.<br/>
+- I can work in a team and independently.<br/>
+- Always in touch
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+',
+
+    ],
+    'education'  => [
+        'title'         => 'Education',
+        'name'          => 'Kazan National Research Technical University named after A. N. Tupolev - "KAI"',
+        'type'          => 'higher (2008-2014)',
+        'qualification' => 'Qualification: engineer-manager',
+    ],
+    'skills'     => [
+        'title'  => 'Skills',
+        'values' => [
+            'Programming languages
+            html, css, php, js, MySql, Java, Kotlin, Python;',
+            'implementation of project tasks;',
+            'implementation of complex integration solutions;',
+            'configuring the server;',
+            'setting up auto-deploy;',
+            'use docker-image;',
+            'database optimization;',
+            'japanese N5;',
+            'technical english',
+        ],
+    ],
+    'work'       => [
+        'experience'       => 'Work experience',
+        'place'            => 'Place of work',
+        'period'           => 'Period of work',
+        'position'         => 'Position',
+        'site'             => 'Site',
+        'responsibilities' => 'Responsibilities',
+        'values'           => [
+            [
+                'name'             => 'Avtodoria',
+                'period'           => 'March 2020 — until now',
+                'position'         => 'full-stack',
+                'link'             => [
+                    'title' => 'itpark-kazan.ru/ru/node/465',
+                    'value' => 'https://www.itpark-kazan.ru/ru/node/465',
+                ],
+                'responsibilities' => [
+                    'Yii2',
+                    'Laravel',
+                    'Jquery',
+                    'Vue.js',
+                    'Refactoring code from Vue with Jquery admixtures to Vue with native Javascript',
+                    'Organizing the move from Yii2 to Laravel',
+                    'Prototyping and UX design',
+                    'Implementation of best modern practices in the code',
+                    'Development of new modules',
+                ],
+            ],
+            [
+                'name'             => 'Codezavod',
+                'period'           => 'May 2019 — February 2020',
+                'position'         => 'PHP middle-developer',
+                'link'             => [
+                    'title' => 'codezavod.ru',
+                    'value' => 'https://codezavod.ru/',
+                ],
+                'responsibilities' => [
+                    'Laravel',
+                    'bitrix',
+                    'Docker',
+                    'setting up auto-deploy;',
+                    'Review code.',
+                    'Automation of internal process',
+                    'Maintaining documentation on projects',
+                    'Optimization code',
+                ],
+            ],
+            [
+                'name'             => 'Wellmedia',
+                'period'           => 'September 2018 — may 2019',
+                'position'         => 'PHP developer',
+                'link'             => [
+                    'title' => 'wellmedia.ru',
+                    'value' => 'https://wellmedia.ru/',
+                ],
+                'responsibilities' => [
+                    'bitrix',
+                    'Site development',
+                    'Technical support site',
+                ],
+            ],
+            [
+                'name'             => 'QSOFT',
+                'period'           => 'January 2018 — may 2018',
+                'position'         => 'PHP junior-developer',
+                'link'             => [
+                    'title' => 'qsoft.ru',
+                    'value' => 'https://qsoft.ru/',
+                ],
+                'responsibilities' => [
+                    'bitrix',
+                    'Search for errors and fix them',
+                    'Development API for mobile application',
+                    'Direct queries to the database',
+                    'Update and insert data from .xls .csv files to database',
+                    'Template integration',
+                    'Development custom component',
+                    'Embedding any solutions in someone else`s code',
+                ],
+            ],
+        ],
+    ],
+];
